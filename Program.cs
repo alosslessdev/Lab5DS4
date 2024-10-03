fba6d0b8500d32174d85fdd394e37319634578fc
FizlyxX <nathancarrasco07@gmail.com>
2024-10-03 14:51:04 -0500

    salsa

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace Lab5DS4
 {
     internal static class Program
     {
+        /// salskadjal
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
